f384b565e6b72661ccbf011f89625ed19b9a21e3
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-04-23 17:46:33 +0100

    get version from __init__

--- setup.py
+++ setup.py
@@ -4,9 +4,26 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def read(rel_path):
+    import os
+    import codecs
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    import os
+    suffix = os.environ["VERSION_SUFFIX" ] if "VERSION_SUFFIX" in os.environ else ""
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1] + suffix
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setuptools.setup(
     name="ir_measures",
-    version="0.0.1",
+    version=get_version("ir_measures/__init__.py"),
     author="Sean MacAvaney",
     author_email="sean.macavaney@glasgow.ac.uk",
     description="provides a common interface to many IR measure tools",
