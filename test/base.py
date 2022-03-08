09697d74f7f799beb778c6bf7bec9fe5e2eddba3
Sean MacAvaney <sean.macavaney@gmail.com>
2022-03-08 11:43:57 +0000

    added missing file

---
+++ test/base.py
@@ -0,0 +1,15 @@
+import unittest
+import itertools
+import ir_measures
+from ir_measures import *
+
+
+class BaseMeasureTest(unittest.TestCase):
+	def assertMetrics(self, a, b, places=4):
+		a = sorted(a)
+		b = sorted(b)
+		self.assertEqual(len(a), len(b))
+		for m0, m1 in zip(a, b):
+			self.assertEqual(m0.query_id, m1.query_id)
+			self.assertEqual(m0.measure, m1.measure)
+			self.assertAlmostEqual(m0.value, m1.value, places=places)
